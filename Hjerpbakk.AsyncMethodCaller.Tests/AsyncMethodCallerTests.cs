ab3b5e7bb38f05ae48ccbaa70ac1d29f517cdd76
Runar Ovesen Hjerpbakk <hjerpbakk@gmail.com>
2015-06-29 14:55:12 +0200

    Changed test framework to NUnit

--- Hjerpbakk.AsyncMethodCaller.Tests/AsyncMethodCallerTests.cs
+++ Hjerpbakk.AsyncMethodCaller.Tests/AsyncMethodCallerTests.cs
@@ -1,177 +1,177 @@
 ﻿using System;
+using NUnit.Framework;
 using Hjerpbakk.AsyncMethodCaller.TestUtility;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Hjerpbakk.AsyncMethodCaller.Tests {
-    [TestClass]
-    public class AsyncMethodCallerTests {
-        private TestAsyncMethodCaller asyncMethodCaller;
-        private IServiceInterface service;
-        private ViewModel viewModel;
+	[TestFixture]
+	public class AsyncMethodCallerTests {
+		TestAsyncMethodCaller asyncMethodCaller;
+		IServiceInterface service;
+		ViewModel viewModel;
 
-        private static bool loadContentCalled;
-        private static bool saveCalled;
+		static bool loadContentCalled;
+		static bool saveCalled;
 
-        [TestCleanup]
-        public void Cleanup() {
-            loadContentCalled = false;
-            saveCalled = false;
-        }
+		[TearDown]
+		public void Cleanup() {
+			loadContentCalled = false;
+			saveCalled = false;
+		}
 
-        [TestMethod]
-        public void CallMethodAndContinue_ServiceWithResultCompletes_ResultIsReturnedAndCallbackCalled() {
-            InitWithService();
+		[Test]
+		public void CallMethodAndContinue_ServiceWithResultCompletes_ResultIsReturnedAndCallbackCalled() {
+			InitWithService();
 
-            viewModel.LoadContent();
+			viewModel.LoadContent();
 
-            Assert.AreEqual("Loading content...", viewModel.MessageToUser);
+			Assert.AreEqual("Loading content...", viewModel.MessageToUser);
 
-            asyncMethodCaller.StartServiceAndWait();
+			asyncMethodCaller.StartServiceAndWait();
 
-            Assert.AreEqual("Content is 1", viewModel.MessageToUser);
-            Assert.IsTrue(loadContentCalled);
-            Assert.IsFalse(saveCalled);
-        }
+			Assert.AreEqual("Content is 1", viewModel.MessageToUser);
+			Assert.IsTrue(loadContentCalled);
+			Assert.IsFalse(saveCalled);
+		}
 
-        [TestMethod]
-        public void CallMethodAndContinue_ServiceWithResultFails_ExceptionIsReturnedAndCallbackCalled() {
-            InitWithFailingService();
+		[Test]
+		public void CallMethodAndContinue_ServiceWithResultFails_ExceptionIsReturnedAndCallbackCalled() {
+			InitWithFailingService();
 
-            viewModel.LoadContent();
+			viewModel.LoadContent();
 
-            Assert.AreEqual("Loading content...", viewModel.MessageToUser);
+			Assert.AreEqual("Loading content...", viewModel.MessageToUser);
 
-            asyncMethodCaller.StartServiceAndWait();
+			asyncMethodCaller.StartServiceAndWait();
 
-            Assert.AreEqual("Loading content failed: LoadingException Message", viewModel.MessageToUser);
-            Assert.IsTrue(loadContentCalled);
-            Assert.IsFalse(saveCalled);
-        }
+			Assert.AreEqual("Loading content failed: LoadingException Message", viewModel.MessageToUser);
+			Assert.IsTrue(loadContentCalled);
+			Assert.IsFalse(saveCalled);
+		}
 
-        [TestMethod]
-        public void CallMethodAndContinue_ServiceCompletes_CallbackCalled() {
-            InitWithService();
+		[Test]
+		public void CallMethodAndContinue_ServiceCompletes_CallbackCalled() {
+			InitWithService();
 
-            viewModel.Save();
+			viewModel.Save();
 
-            Assert.AreEqual("Saving content...", viewModel.MessageToUser);
+			Assert.AreEqual("Saving content...", viewModel.MessageToUser);
 
-            asyncMethodCaller.StartServiceAndWait();
+			asyncMethodCaller.StartServiceAndWait();
 
-            Assert.AreEqual("Content saved", viewModel.MessageToUser);
-            Assert.IsTrue(saveCalled);
-            Assert.IsFalse(loadContentCalled);
-        }
+			Assert.AreEqual("Content saved", viewModel.MessageToUser);
+			Assert.IsTrue(saveCalled);
+			Assert.IsFalse(loadContentCalled);
+		}
 
-        [TestMethod]
-        public void CallMethodAndContinue_ServiceFails_ExceptionIsReturnedAndCallbackCalled() {
-            InitWithFailingService();
+		[Test]
+		public void CallMethodAndContinue_ServiceFails_ExceptionIsReturnedAndCallbackCalled() {
+			InitWithFailingService();
 
-            viewModel.Save();
+			viewModel.Save();
 
-            Assert.AreEqual("Saving content...", viewModel.MessageToUser);
+			Assert.AreEqual("Saving content...", viewModel.MessageToUser);
 
-            asyncMethodCaller.StartServiceAndWait();
+			asyncMethodCaller.StartServiceAndWait();
 
-            Assert.AreEqual("Saving content failed: SavingException Message", viewModel.MessageToUser);
-            Assert.IsTrue(saveCalled);
-            Assert.IsFalse(loadContentCalled);
-        }
+			Assert.AreEqual("Saving content failed: SavingException Message", viewModel.MessageToUser);
+			Assert.IsTrue(saveCalled);
+			Assert.IsFalse(loadContentCalled);
+		}
 
-        private void InitWithService() {
-            service = new ServiceImplmentation();
-            Init();
-        }
+		void InitWithService() {
+			service = new ServiceImplmentation();
+			Init();
+		}
 
-        private void InitWithFailingService() {
-            service = new FailingServiceImplementation();
-            Init();
-        }
+		void InitWithFailingService() {
+			service = new FailingServiceImplementation();
+			Init();
+		}
 
-        private void Init() {
-            // The TestAsyncMethodCaller can wait for asynchronous calls to complete. 
-            asyncMethodCaller = new TestAsyncMethodCaller();
-            viewModel = new ViewModel(asyncMethodCaller, service);
-        }
+		void Init() {
+			// The TestAsyncMethodCaller can wait for asynchronous calls to complete. 
+			asyncMethodCaller = new TestAsyncMethodCaller();
+			viewModel = new ViewModel(asyncMethodCaller, service);
+		}
 
-        private class ViewModel {
-            private readonly AsyncMethodCaller asyncMethodCaller;
-            private readonly IServiceInterface service;
+		class ViewModel {
+			readonly AsyncMethodCaller asyncMethodCaller;
+			readonly IServiceInterface service;
 
-            public string MessageToUser { get; private set; }
+			public string MessageToUser { get; private set; }
 
-            /// <summary>
-            /// The dependencies are injected.
-            /// </summary>
-            /// <param name="asyncMethodCaller">Use TestAsyncMethodCaller for tests, AsyncMethodCaller in production.</param>
-            /// <param name="service">Inject the service for easier faking and faster tests.</param>
-            public ViewModel(AsyncMethodCaller asyncMethodCaller, IServiceInterface service) {
-                this.asyncMethodCaller = asyncMethodCaller;
-                this.service = service;
-            }
+			/// <summary>
+			/// The dependencies are injected.
+			/// </summary>
+			/// <param name="asyncMethodCaller">Use TestAsyncMethodCaller for tests, AsyncMethodCaller in production.</param>
+			/// <param name="service">Inject the service for easier faking and faster tests.</param>
+			public ViewModel(AsyncMethodCaller asyncMethodCaller, IServiceInterface service) {
+				this.asyncMethodCaller = asyncMethodCaller;
+				this.service = service;
+			}
 
-            public void LoadContent() {
-                MessageToUser = "Loading content...";
-                
-                // Separate methods for each callback.
-                asyncMethodCaller.CallMethodAndContinue(() => service.LoadContent(),
-                    LoadContentCompleted,
-                    LoadContentFailed);
-            }
+			public void LoadContent() {
+				MessageToUser = "Loading content...";
 
-            public void Save() {
-                MessageToUser = "Saving content...";
+				// Separate methods for each callback.
+				asyncMethodCaller.CallMethodAndContinue(() => service.LoadContent(),
+					LoadContentCompleted,
+					LoadContentFailed);
+			}
 
-                // Example of inline callbacks.
-                asyncMethodCaller.CallMethodAndContinue(() => service.Save(),
-                    () => { MessageToUser = "Content saved"; },
-                    exception => { MessageToUser = "Saving content failed: " + exception.InnerException.Message; });
-            }
+			public void Save() {
+				MessageToUser = "Saving content...";
 
-            private void LoadContentCompleted(int content) {
-                MessageToUser = "Content is " + content;
-            }
+				// Example of inline callbacks.
+				asyncMethodCaller.CallMethodAndContinue(service.Save,
+					() => { MessageToUser = "Content saved"; },
+					exception => { MessageToUser = "Saving content failed: " + exception.InnerException.Message; });
+			}
 
-            private void LoadContentFailed(Exception exception) {
-                MessageToUser = "Loading content failed: " + exception.InnerException.Message;
-            }
-        }
+			void LoadContentCompleted(int content) {
+				MessageToUser = "Content is " + content;
+			}
 
-        /// <summary>
-        /// Example of a service interface.
-        /// </summary>
-        private interface IServiceInterface {
-            int LoadContent();
-            void Save();
-        }
+			void LoadContentFailed(Exception exception) {
+				MessageToUser = "Loading content failed: " + exception.InnerException.Message;
+			}
+		}
 
-        /// <summary>
-        /// Example of failing service calls.
-        /// </summary>
-        private class FailingServiceImplementation : IServiceInterface {
-            public int LoadContent() {
-                loadContentCalled = true;
-                throw new Exception("LoadingException Message");
-            }
+		/// <summary>
+		/// Example of a service interface.
+		/// </summary>
+		interface IServiceInterface {
+			int LoadContent();
+			void Save();
+		}
 
-            public void Save() {
-                saveCalled = true;
-                throw new Exception("SavingException Message");
-            }
-        }
+		/// <summary>
+		/// Example of failing service calls.
+		/// </summary>
+		class FailingServiceImplementation : IServiceInterface {
+			public int LoadContent() {
+				loadContentCalled = true;
+				throw new Exception("LoadingException Message");
+			}
 
-        /// <summary>
-        /// Example of successful service calls.
-        /// </summary>
-        private class ServiceImplmentation : IServiceInterface {
-            public int LoadContent() {
-                loadContentCalled = true;
-                return 1;
-            }
+			public void Save() {
+				saveCalled = true;
+				throw new Exception("SavingException Message");
+			}
+		}
 
-            public void Save() {
-                saveCalled = true;
-            }
-        }
-    }
+		/// <summary>
+		/// Example of successful service calls.
+		/// </summary>
+		class ServiceImplmentation : IServiceInterface {
+			public int LoadContent() {
+				loadContentCalled = true;
+				return 1;
+			}
+
+			public void Save() {
+				saveCalled = true;
+			}
+		}
+	}
 }
